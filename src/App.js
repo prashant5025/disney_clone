5d8e2ea225e7bc080a899b13f37daaebedad25b4
Prashant Singh <psingh5025615@gmail.com>
2022-09-19 17:36:55 +0530

    allinone section is also done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import AllinOne from "./components/AllinOne";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Support from "./components/Support";
@@ -10,6 +11,7 @@ function App() {
       <Hero/>
       <About/>
       <Support/>
+      <AllinOne/>
     </div>
   );
 }
--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -5,7 +5,7 @@ import {ChipIcon, SupportIcon} from '@heroicons/react/solid'
 import supportImg from '../assets/img/support.jpg';
 const Support = () => {
   return (
-    <div className="w-full h-screen mt-24">
+    <div className="w-full mt-24">
         <div className="w-full h-[700px] bg-gray-900/90 absolute">
             <img className="w-full h-full object-cover mix-blend-overlay" src={supportImg} alt="/" />
         </div>
