c2a603908e3f280b352d38f93d99a45a78034082
Prashant Singh <psingh5025615@gmail.com>
2022-09-20 09:46:10 +0530

    smooth scroll added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import { Link} from 'react-scroll'
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -12,17 +12,15 @@ const Navbar = () => {
     <div className="w-full h-[80px] z-10 bg-zinc-200 fixed drop-shadow-lg">
       <div className="px-2 flex justify-between items-center w-full h-full">
         <div className="flex items-center">
-          <h1 className="text-3xl font-bold mr-4 sm:text-4xl hover:text-indigo-600 cursor-pointer">BRAND.</h1>
+          <h1 className="text-3xl font-bold mr-4 sm:text-4xl hover:text-indigo-600 cursor-pointer"><Link to='home' spy={true} smooth={true} offset={50} duration={1000}>BRAND.</Link></h1>
 
           <ul className="hidden md:flex">
-          <li><Link to='text1' spy={true} smooth={true} offset={50} duration={500}>Home</Link></li>
+          <li className=" p-4 hover:text-indigo-600 cursor-pointer"><Link to='home' spy={true} smooth={true} offset={50} duration={1000}>Home</Link></li>
+          <li className=" p-4 hover:text-indigo-600 cursor-pointer"><Link to='about' spy={true} smooth={true} offset={50} duration={1000}>About</Link></li>
+          <li className=" p-4 hover:text-indigo-600 cursor-pointer"><Link to='support' spy={true} smooth={true} offset={50} duration={1000}>Support</Link></li>
+          <li className=" p-4 hover:text-indigo-600 cursor-pointer"><Link to='platforms' spy={true} smooth={true} offset={50} duration={1000}>Platforms</Link></li>
+          <li className=" p-4 hover:text-indigo-600 cursor-pointer"><Link to='pricing' spy={true} smooth={true} offset={50} duration={1000}>Pricing</Link></li>
             
-
-            <li className=" p-4 hover:text-indigo-600 cursor-pointer">Home</li>
-            <li className=" p-4 hover:text-indigo-600 cursor-pointer">About</li>
-            <li className=" p-4 hover:text-indigo-600 cursor-pointer">Support</li>
-            <li className=" p-4 hover:text-indigo-600 cursor-pointer">Platforms</li>
-            <li className=" p-4 hover:text-indigo-600 cursor-pointer">Pricing</li>
           </ul>
         </div>
         <div className="hidden md:flex">
@@ -40,11 +38,11 @@ const Navbar = () => {
         </div>
       </div>
       <ul className={!nav ? "hidden" : "absolute bg-zinc-200 w-full px-8"}>
-        <li className="border-b-2 border-zinc-300 w-full">Home</li>
-        <li className="border-b-2 border-zinc-300 w-full">About</li>
-        <li className="border-b-2 border-zinc-300 w-full">Support</li>
-        <li className="border-b-2 border-zinc-300 w-full">Platforms</li>
-        <li className="border-b-2 border-zinc-300 w-full">Pricing</li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link to='home' spy={true} smooth={true} offset={50} duration={1000}>Home</Link></li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link to='about' spy={true} smooth={true} offset={50} duration={1000}>About</Link></li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link to='support' spy={true} smooth={true} offset={50} duration={1000}>Support</Link></li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link to='platforms' spy={true} smooth={true} offset={50} duration={1000}>Platforms</Link></li>
+        <li className="border-b-2 border-zinc-300 w-full"><Link to='pricing' spy={true} smooth={true} offset={50} duration={1000}>Pricing</Link></li>
         <div className="flex flex-col my-4">
           <button className="bg-transparent text-indigo-600 px-8 py-3 mb-4">
             Sign In
